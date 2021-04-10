a359caf91ba1a4e9a3b2c8755f8083d957f40e51
SelmaInce1 <milaevans2000@icloud.com>
2021-04-10 18:36:36 -0500

    Somecommon  pages are created

---
+++ src/test/java/com/prlbank/pages/PRLCustomersPage.java
@@ -0,0 +1,18 @@
+package com.prlbank.pages;
+
+import com.prlbank.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PRLCustomersPage {
+
+    public PRLCustomersPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id="jh-create-entity")
+    public WebElement createANewCustomerButton;
+
+
+}
